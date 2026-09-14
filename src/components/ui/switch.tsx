d0506b7ff2a0f7bc3997d984b7d341535998e5c2
Nezumi-2711 <phanhuuloi15@gmail.com>
2026-09-14 16:52:24 +0700

    fix: improve style for setting pages

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -8,8 +8,8 @@ function Switch({ className, ...props }: React.ComponentProps<typeof SwitchPrimi
 		<SwitchPrimitive.Root
 			data-slot="switch"
 			className={cn(
-				'peer inline-flex h-5.5 w-10 shrink-0 cursor-pointer items-center rounded-full border border-transparent bg-[#dfdfdf] p-px transition-colors outline-none',
-				'data-[state=checked]:border-[#35c586] data-[state=checked]:bg-(--primary)',
+				'peer inline-flex h-5.5 w-10 shrink-0 cursor-pointer items-center rounded-full border border-transparent bg-[#dfdfdf] p-px transition-colors outline-none dark:bg-white/20',
+				'data-[state=checked]:border-[#35c586] data-[state=checked]:bg-(--primary) dark:data-[state=checked]:border-[#35c586] dark:data-[state=checked]:bg-(--primary)',
 				'focus-visible:shadow-[0_0_0_3px_rgb(36_180_126/0.14)] focus-visible:outline-none!',
 				'disabled:cursor-not-allowed disabled:opacity-50',
 				className,
@@ -18,7 +18,7 @@ function Switch({ className, ...props }: React.ComponentProps<typeof SwitchPrimi
 		>
 			<SwitchPrimitive.Thumb
 				data-slot="switch-thumb"
-				className="pointer-events-none block size-4.5 translate-x-0 rounded-full bg-white shadow-[0_1px_2px_rgb(0_0_0/0.15)] transition-transform data-[state=checked]:translate-x-4.5"
+				className="pointer-events-none block size-4.5 translate-x-0 rounded-full bg-white shadow-[0_1px_2px_rgb(0_0_0/0.15)] transition-transform data-[state=checked]:translate-x-4.5 dark:bg-neutral-100"
 			/>
 		</SwitchPrimitive.Root>
 	);
